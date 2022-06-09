bd28215baeb9511743594d89f14327ae1c56895e
mohamad-arabi <mohamad.arabi@databricks.com>
2022-06-09 15:48:55 -0700

    Create test-run.py

---
+++ test-run.py
@@ -0,0 +1,18 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+import mlflow
+# enable autologging
+mlflow.set_experiment(experiment_id='6df1c4777c3b4d869f6a21689dbcd38e')
+mlflow.sklearn.autolog()
+
+# prepare training data
+X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
+y = np.dot(X, np.array([1, 2])) + 3
+# train a model
+model = LinearRegression()
+with mlflow.start_run() as run:
+    model.fit(X, y)
+    mlflow.log_params({"param key": "param val 2"})
+    mlflow.log_metrics({"metric name": 2})
+    mlflow.set_tags({"tag key": "tag val 2"})
